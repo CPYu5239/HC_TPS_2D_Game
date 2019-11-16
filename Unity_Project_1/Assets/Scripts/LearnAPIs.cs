268fd9f3f32f30b8f7296530b9627e9aefab0ae5
CPYu5239 <57282801+CPYu5239@users.noreply.github.com>
2019-11-16 17:06:18 +0800

    使用API

---
+++ Unity_Project_1/Assets/Scripts/LearnAPIs.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class LearnAPIs : MonoBehaviour
+{
+    //非靜態成員使用方法  需要先宣告欄位存放實體物件
+    public Transform TR;
+    //最常用
+    private Camera CR;
+    private AudioSource AS;
+
+    private void Start()
+    {
+        //使用靜態成員(直接用) - 使用屬性
+        print("隨機值 :" + Random.value);
+
+        //使用 數學類別的PI成員
+        print("PI :" + Mathf.PI);
+
+        //使用使用靜態成員 - 方法
+        print("使用方法Range :" + Random.Range(1f, 10f));
+
+        print("使用方法絕對值 :" + Mathf.Abs(-10));
+
+        /*非靜態成員使用方法  需要先宣告欄位存放實體物件(如宣告Public需要在Unity內部先設定，如果要在方法內宣告則不需加修飾詞，
+        但要用GameObject.Find(ObjectName).GetComponent<ClassType>()來抓取專案內的物件)*/
+        print("物件的座標(由Unity內部設定，不能是空值) : " + TR.position);
+
+        //指定Unity內部的物件到CR
+        CR = GameObject.Find("Main Camera").GetComponent<Camera>();
+        print("Camera的深度 : " + CR.depth);
+
+        AS = GameObject.Find("BGM").GetComponent<AudioSource>();
+        AS.Stop();
+    }
+
+    private void Update()
+    {
+        //直接宣告的區域類別
+        Transform Bird = GameObject.Find("bird").GetComponent<Transform>();
+        Bird.Rotate(0, 0, 1000);
+    }
+}
---
+++ Unity_Project_1/Assets/Scripts/LearnIF.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+public class LearnIF : MonoBehaviour
+{
+    public bool test;
+    public string prop;
+    public int HP = 100;
+    private void Update()
+    {
+        #region IF判斷式語法
+        //IF判斷式語法
+        //if(bool == true) { 陳述式 }
+        if (test)
+        {
+            print("打開開關~");
+        }
+        //(bool == false) 才執行else
+        else
+        {
+            print("關閉開關~");
+        }
+
+        //多層判斷式
+        if (prop == "red")
+        {
+            print("HP++");
+        }
+        else if(prop == "blue")
+        {
+            print("MP++");
+        }
+        else
+        {
+            print("啥都沒~");
+        }
+        #endregion
+
+        if (HP <= 20)
+        {
+            print("快死了!!");
+        }
+        else if (HP <= 50)
+        {
+            print("危險!");
+        }
+        else if (HP <= 70)
+        {
+            print("警告");
+        }
+        else
+        {
+            print("沒4~");
+        }
+    }
+}
