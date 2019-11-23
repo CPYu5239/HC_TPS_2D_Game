c6fa06be5ed8797a6921f13287387455dcf4bfda
CPYu5239 <57282801+CPYu5239@users.noreply.github.com>
2019-11-23 15:29:08 +0800

    完成小雞跳躍，鋼體reset等讓操作變順暢

--- Unity_Project_1/Assets/Scripts/Bird.cs
+++ Unity_Project_1/Assets/Scripts/Bird.cs
@@ -3,17 +3,28 @@
 public class Bird : MonoBehaviour
 {
     [Header("跳躍高度")]
-    [Range(1.0f, 10.0f)]
+    [Range(1.0f, 1000.0f)]
     public float jumpHeight = 1.0f; //跳躍高度
     [Header("是否死亡")]
     public bool death; //是否死亡
-
+    [Header("鋼體")]
+    Rigidbody2D rig2D;
+    
     /// <summary>
     /// 小雞跳躍的函式
     /// </summary>
     private void Jump()
     {
-
+        //按下左鍵 --> 小雞往上
+        //輸入.按下按鍵(案件列舉.左鍵)(手機觸控)
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            rig2D = GameObject.Find("bird").GetComponent<Rigidbody2D>();
+            rig2D.Sleep();
+            //print("按左鍵");
+            rig2D.gravityScale = 1;  //更改小雞鋼體重力指數
+            rig2D.AddForce(new Vector2(0, jumpHeight));
+        }
     }
 
     /// <summary>
@@ -26,4 +37,9 @@ public class Bird : MonoBehaviour
         return death;
     }
     
+
+    private void Update()
+    {
+        Jump();
+    }
 }
